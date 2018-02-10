374e6165a6ff7a0866fa2b482ed4b2191cfd94af
ma1ive <ma1ive@qq.com>
2018-02-10 11:09:20 +0800

    更新国内CDN

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const HOSTNAME_WHITELIST = [
   self.location.hostname,
   "huangxuan.me",
   "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "bootcdn.cn"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
